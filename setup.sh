c7556084360d6bcc910e9c1d9d58146762edfa3a
matsudan <daaamatsun@gmail.com>
2023-08-20 04:54:13 +0900

    Fix symbolic link target

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-DOTFILES=(.zshrc .tmux.conf .vimrc)
-
+DOTFILES=(.zshrc .vimrc)
+SCRIPT_DIR=$(cd $(dirname $0);pwd)
 for file in ${DOTFILES[@]}
 do
-    ln -s $HOME/dotfiles/$file $HOME/$file
+    ln -s $SCRIPT_DIR/$file $HOME/$file
 done
